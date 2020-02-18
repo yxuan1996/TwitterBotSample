c77bc5a5bb01942f7a06bd73881cc213c3e7c641
Unknown <yxuan1996@gmail.com>
2020-02-18 10:07:40 +0000

    Change index.js

--- GenerateNonsense/index.js
+++ GenerateNonsense/index.js
@@ -1,9 +1,9 @@
-module.exports = async function (context, req) {
+/* module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
-            // status: 200, /* Defaults to 200 */
+            // status: 200,  Defaults to 200 
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
@@ -13,4 +13,13 @@ module.exports = async function (context, req) {
             body: "Please pass a name on the query string or in the request body"
         };
     }
+}; */
+
+//We change the function so that it always returns the nonsense word "Bibble wibble wobble"
+module.exports = async function (context, req) {
+    //This line logs a message internally in azure
+    context.log('Generating Nonsense...');
+    context.res = {
+        body: "Bibble wibble wobble"
+    };
 };
